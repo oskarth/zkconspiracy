59128fb7d0e21eed43a01f468c0236ae7e03bd18
Oskar Thoren <ot@oskarthoren.com>
2022-04-01 15:16:02 +0800

    chore: Add typescript generated bindings

--- types/index.ts
+++ types/index.ts
@@ -2,5 +2,13 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { Verifier } from "./Verifier";
+export type { Hasher } from "./Hasher";
+export type { MerkleTreeWithHistory } from "./MerkleTreeWithHistory";
+export type { IVerifier } from "./IVerifier";
+export type { ZKConspiracy } from "./ZKConspiracy";
 
 export { Verifier__factory } from "./factories/Verifier__factory";
+export { Hasher__factory } from "./factories/Hasher__factory";
+export { MerkleTreeWithHistory__factory } from "./factories/MerkleTreeWithHistory__factory";
+export { IVerifier__factory } from "./factories/IVerifier__factory";
+export { ZKConspiracy__factory } from "./factories/ZKConspiracy__factory";
